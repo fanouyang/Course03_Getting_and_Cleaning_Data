123058883baa1d828860c1f969c05d35758b96bd
Fan Ouyang <ouyan064@umn.edu>
2017-08-21 16:44:42 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,62 +1,50 @@
-library(plyr)
-
 # download files in work directory
+filename <- "getdata_dataset.zip"
 
-# Step 1 Merge the training and test sets to create one data set
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
 
-x_train <- read.table("train/X_train.txt")
-y_train <- read.table("train/y_train.txt")
-subject_train <- read.table("train/subject_train.txt")
+# Load activity labels + features
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
 
-x_test <- read.table("test/X_test.txt")
-y_test <- read.table("test/y_test.txt")
-subject_test <- read.table("test/subject_test.txt")
+# Extract only the data on mean and standard deviation
+featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
+featuresWanted.names <- features[featuresWanted,2]
+featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names) # replace '-mean' with 'Mean'
+featuresWanted.names = gsub('-std', 'Std', featuresWanted.names) # replace '-std' with 'Std'
+featuresWanted.names <- gsub('[-()]', '', featuresWanted.names) # replace
 
-# create 'x' data set
-x_data <- rbind(x_train, x_test)
+# Load the datasets
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
 
-# create 'y' data set
-y_data <- rbind(y_train, y_test)
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
+testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
 
-# create 'subject' data set
-subject_data <- rbind(subject_train, subject_test)
+# merge datasets and add labels
+allData <- rbind(train, test)
+colnames(allData) <- c("subject", "activity", featuresWanted.names)
 
-# Step 2 Extract mean and standard deviation for each measurement
-features <- read.table("features.txt")
+# turn activities & subjects into factors
+allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+allData$subject <- as.factor(allData$subject)
 
-# get only columns mean() or std() 
-mean_std <- grep("-(mean|std)\\(\\)", features[, 2])
+library(reshape2)
+allData.melted <- melt(allData, id = c("subject", "activity")) # wide to long format
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, fun.aggregate=mean, na.rm=TRUE) #long to wide format
 
-# subset the desired columns
-x_data <- x_data[, mean_std]
-
-# correct the column names
-names(x_data) <- features[mean_std, 2]
-
-# Step 3 Use descriptive activity names to name the activities in the data set
-activities <- read.table("activity_labels.txt")
-
-# update values with correct activity names
-y_data[, 1] <- activities[y_data[, 1], 2]
-
-# correct column name
-names(y_data) <- "activity"
-
-# Step 4 Appropriately label the data set with descriptive variable names
-# correct column name
-names(subject_data) <- "subject"
-
-# bind all the data in a single data set
-all_data <- cbind(x_data, y_data, subject_data)
-
-# Step 5 Create a second, independent tidy data set with the average of each variable
-# for each activity and each subject
-
-# 66 <- 68 columns but last two (activity & subject)
-averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
-
-# create a new table text file
-write.table(averages_data, "averages_data.txt", row.name=FALSE)
-
-# check new data frame
-averages_data
+#save data 
+write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
